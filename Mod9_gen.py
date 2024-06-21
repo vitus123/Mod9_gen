74975a51e8c8d0e1feb5eda7874b91661fc751f6
vitus123 <vitus123@mail.ru>
2024-06-21 22:39:46 +0700

    Add files via upload

---
+++ Mod9_gen.py
@@ -0,0 +1,14 @@
+
+def all_variants(s):
+    n = len(s)
+    for i in range(1,n):
+        subset = ""
+        for j in range(n):
+            if (i >j) > 0:
+                subset += s[j]
+        yield subset
+
+
+a = all_variants("abc")
+for i in a:
+    print(i)
